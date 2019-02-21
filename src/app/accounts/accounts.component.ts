b7d27a87769313acd479aea3679b86fc797428ae
Abbas Nazar <abbasnazar.970@gmail.com>
2019-02-21 16:58:49 +0500

    Local accounts update

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../services/account.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from '../services/account.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 import {HttpErrorResponse} from '@angular/common/http';
 import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
@@ -30,17 +31,17 @@ export class AccountsComponent implements OnInit {
           // updating account list
           this.account.getAccounts();
           this.router.navigateByUrl('Dashboard');
+        }, (err: HttpErrorResponse) => {
+          Swal.fire('Shame on us', 'Unable to add account', 'error');
+          console.log(err);
+          console.log(err.name);
+          console.log(err.message);
+          console.log(err.status);
         });
       } else {
         this.account.getAccounts();
         this.router.navigateByUrl('Dashboard');
       }
-    }, (err: HttpErrorResponse) => {
-      Swal.fire('Shame on us', 'Unable to add account', 'error');
-        console.log(err);
-        console.log(err.name);
-        console.log(err.message);
-        console.log(err.status);
     });
 
     // setting user account array
@@ -60,7 +61,7 @@ export class AccountsComponent implements OnInit {
     if (values.currentTarget.checked)
       this.accountsToMerge.push(accountId);
     else
-      this.accountsToMerge = this.accountsToMerge.filter(id => id !== accountId)
+      this.accountsToMerge = this.accountsToMerge.filter(id => id !== accountId);
   }
 
   // method for adding client drive
@@ -82,7 +83,12 @@ export class AccountsComponent implements OnInit {
   // method for removing an account
   removeAccount(id) {
     this.account.deleteAccount(id).subscribe((data) => {
-      this.account.getAccounts();
+      this.accounts = this.accounts.filter(function (value, index, arr) {
+
+        return value['_id'] !== id;
+
+      });
+      // this.account.getAccounts();
     }, (err: HttpErrorResponse) => {
       Swal.fire('Shame on us', 'Unable to unlink account', 'error');
       console.log(err);
@@ -93,11 +99,17 @@ export class AccountsComponent implements OnInit {
   }
 
   mergeAccounts() {
-    if (this.accountsToMerge.length >= 2){
+    if (this.accountsToMerge.length >= 2) {
       this.account.changeMergeStatus(this.accountsToMerge, true).subscribe((data) => {
-        this.account.getAccounts();
+        this.accountsToMerge.forEach((value) => {
+          this.accounts.filter((value1) => {
+            if (value1['_id'] === value) {
+              value1.merged = true;
+            }
+          });
+        });
         this.accountsToMerge = [];
-      } ,(err: HttpErrorResponse) => {
+      }, (err: HttpErrorResponse) => {
         Swal.fire('Shame on us', 'Unable to merge accounts', 'error');
         console.log(err);
         console.log(err.name);
@@ -111,7 +123,7 @@ export class AccountsComponent implements OnInit {
 
   demergeAccounts() {
     this.account.changeMergeStatus(this.getMergedAccounts(), false).subscribe((data) => {
-      this.account.getAccounts();
+      this.accounts.filter(value => value.merged = false)
       this.accountsToMerge = [];
     }, (err: HttpErrorResponse) => {
       Swal.fire('Shame on us', 'Unable to demerge accounts', 'error');
@@ -122,11 +134,11 @@ export class AccountsComponent implements OnInit {
     });
   }
 
-  getSizeInGb(size){
-    return (size/Math.pow(1024,3)).toFixed(2);
+  getSizeInGb(size) {
+    return (size / Math.pow(1024, 3)).toFixed(2);
   }
 
-  getMergedAccountsStorage(){
+  getMergedAccountsStorage() {
 
     var total = 0;
     var used = 0;
@@ -135,7 +147,7 @@ export class AccountsComponent implements OnInit {
       used += Number(account.storage.used);
     });
 
-    return {used: (used/Math.pow(1024,3)).toFixed(2), total: (total/Math.pow(1024,3)).toFixed(2)};
+    return {used: (used / Math.pow(1024, 3)).toFixed(2), total: (total / Math.pow(1024, 3)).toFixed(2)};
   }
 }
 
