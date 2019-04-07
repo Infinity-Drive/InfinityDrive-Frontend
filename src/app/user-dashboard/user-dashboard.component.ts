24c4e89c822e4053d997c08e1be4dcd93a019d2c
Abbas Nazar <abbasnazar.970@gmail.com>
2019-04-07 16:36:57 +0500

    accounts Limit in navbar

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -12,14 +12,24 @@ export class UserDashboardComponent implements OnInit {
   userName: string;
   isOpened = false;
   accounts = [];
+  totalAccounts;
+  accountsLimit = 5;
 
   constructor(private route: Router, private account: AccountService) {
   }
 
   ngOnInit() {
     this.userName = localStorage.getItem('infinityName');
-    this.account.accountsToBeEmited.subscribe((value) => {
-      this.accounts = value;
+    this.account.accountsToBeEmited.subscribe((value: any[]) => {
+      let count = 0;
+      this.totalAccounts = value.length;
+      this.accounts = value.filter((value1) => {
+        count++;
+        //  accounts to be displayed
+        if (count <= this.accountsLimit) {
+          return value1;
+        }
+      });
     });
   }
 
