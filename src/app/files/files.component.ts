cbed61ccbd890e3d7e92847a8c026751ec0a58d2
mhd3v <mamir3012@gmail.com>
2019-04-20 15:54:25 +0500

    Delete file swal fix styling

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -208,12 +208,12 @@ export class FilesComponent implements OnInit {
   deleteFile(file) {
 
     Swal.fire({
-      title: `Are you sure you want to delete ${file.name}?`,
-      text: 'You won\'t be able to revert this!',
+      title: `Are you sure?`,
+      text: `You won\'t be able to recover ${file.name} once deleted!`,
       type: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
       confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
       if (result.value) {
